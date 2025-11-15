af74038a33561f050bb69549d33bfdcd3a96c54f
Andres Rojas <andresgustavorojas0@gmail.com>
2025-11-15 12:20:56 -0300

    feat: favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy({"src/favicon/": "/"});
     return {
         dir: {
             input: "src",
