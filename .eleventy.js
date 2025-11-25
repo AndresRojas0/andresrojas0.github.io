9efbc3f36e2c2f542256bfe3cf2593adaf02df7d
Andres Rojas <andresgustavorojas0@gmail.com>
2025-11-24 22:37:43 -0300

    add: content

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,7 @@
-const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
-
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy({"src/favicon/": "/"});
-    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
     return {
         dir: {
             input: "src",
